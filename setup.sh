c066a2375df0b80ee3924fa3db3266dc4b29e3e0
Kelong Wang <kelong.wang@stonybrook.edu>
2014-05-28 21:21:16 -0400

    add tmux config and bash alias

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# tmux
+ln -s $DIR/tmux.conf ~/.tmux.conf
+
+# alias
+# for bash currently
+cat <<EOF >> ~/.bashrc
+if [ -f $DIR/myshell ]; then
+	. $DIR/myshell
+fi
+EOF
