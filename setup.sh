dba9818ce60fe3bd8039cf5a3e652643aa6fa989
Kelong Wang <buaawkl@gmail.com>
2018-03-13 23:59:10 +0000

    Renew ssh agent in the tmux session upon relogin

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # tmux
 ln -s $DIR/tmux.conf ~/.tmux.conf
 
+# ssh
+ln -s $DIR/sshrc ~/.ssh/rc
+
 # alias
 # for bash currently
 cat <<EOF >> ~/.bash_profile
